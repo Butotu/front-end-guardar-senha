17f3c95cfaeff1ce7450631d5db234200cdafa3b
Butotu <davilamarcaemail@gmail.com>
2026-05-14 17:28:44 -0300

    add

---
+++ src/App.jsx
@@ -0,0 +1,94 @@
+import { useState } from "react";
+import { login } from "./services/loginService";
+function App() {
+
+  const [usuario, setUsuario] = useState("");
+  const [senha, setSenha] = useState("");
+
+  async function loginFront() {
+    try {
+      const data = await login(usuario, senha);
+      console.log("logado", data);
+    } catch (error) {
+      console.error("Erro ao logar:", error);
+    }
+
+  }
+
+  return (
+    <div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: "10px",
+          padding: "20px",
+        }}
+      >
+        <h1
+          style={{
+            color: "#222",
+            fontSize: "2.5rem",
+            fontWeight: "bold",
+            margin: 0,
+          }}
+        >
+          Gerenciador de Senhas
+        </h1>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "12px",
+            width: "300px",
+          }}
+        >
+          <input
+            type="text"
+            placeholder="Usuário"
+            value={usuario}
+            onChange={(e) => setUsuario(e.target.value)}
+            style={{
+              padding: "10px",
+              borderRadius: "8px",
+              border: "1px solid #ccc",
+              outline: "none",
+            }}
+          />
+
+          <input
+            type="password"
+            value={senha}
+            onChange={(e) => setSenha(e.target.value)}
+            placeholder="Senha"
+            style={{
+              padding: "10px",
+              borderRadius: "8px",
+              border: "1px solid #ccc",
+              outline: "none",
+            }}
+          />
+
+          <button
+            onClick={loginFront}
+            style={{
+              padding: "10px",
+              borderRadius: "8px",
+              border: "none",
+              backgroundColor: "#4CAF50",
+              color: "white",
+              fontWeight: "bold",
+              cursor: "pointer",
+            }}
+          >
+            Entrar
+          </button>
+        </div>
+      </div>
+    </div >
+  )
+}
+
+export default App
---
+++ src/services/loginService.js
@@ -0,0 +1,19 @@
+const api = "http://localhost:3000"
+
+export async function login(usuario, senha) {
+
+    const res = await fetch(`${api}/login`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ nome: usuario, senha })
+    })
+    const data = await res.json()
+
+    if (!res.ok) {
+        throw new Error("Erro no login")
+    }
+
+    return data;
+}
